8ab288cade20204df28aaae58dd16af55f5b12cf
Vaishnavi-eng137 <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-08 15:03:20 +0530

    Assignment and comparison operators

--- python_operators.py
+++ python_operators.py
@@ -33,3 +33,45 @@ print("full name  is  = ",full_name)
 
 multiply_num_str = "vaisu"*5 # this works & gives vaisu 5 times
 print(multiply_num_str)
+
+# Assignment operators
+# = , x = 5
+# +=,x += 5 --> x = x+5
+# -=,x -= 5 --> x = x-5
+# *=,x *= 5 --> x = x*5
+# /=,x /= 5 --> x = x/5
+# //=,x //= 5 --> x = x//5
+# %=,x %= 5 --> x = x%5
+
+# Comparison operators (we compare operands values)
+# ==, Equals to condition, x == y
+# !=, not Equals to condition, x != y
+# <, Less than condition, x < y
+# <=, Less than or equal to condition, x <= y
+# >,Greater than condition, x > y
+# >=,Greater than or equal to condition, x >= y
+
+a = 22
+b = 20
+
+print("result of a==b ", a == b)
+print("result of a!=b ", a != b)
+print("result of a<=b ", a <= b)
+print("result of a>=b ", a >= b)
+print("result of a>b ", a > b)
+print("result of a<b ", a < b)
+
+# logical operators in Python (logical check will happen for expression result)
+# i.e comparing the values of two expression
+# and --> returns true if both statements are true and vice versa
+# or --> returns true if one of statements are true 
+# not --> reverse the result, returns false if the result is true
+
+m = 10
+n = 67
+print('m>10 and n<10 result', m>10 and n<10) # false and false --> false
+print('m>10 or n>10 result', m>10 or n>10) # false or true --> true
+print("not(m>10 and n<10) result", not(m<10 and n<10)) # not(true and false) -->true
+
+
+
