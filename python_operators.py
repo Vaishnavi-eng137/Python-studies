a3a44c0b9c3dfa57f7dd3fb1e086f6427abc677c
Vaishnavi-eng137 <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-08 14:37:22 +0530

    string operators

--- python_operators.py
+++ python_operators.py
@@ -17,3 +17,19 @@ print("float division of x/y = ",x/y)
 print("integer division of x//y = ",x//y)
 print("modulus of x%y = ",x%y)
 print("power of y on x = ",x**y)
+
+# some operations on string
+str_data ='vaishnavi'
+empty_str = ''
+
+# concat operatios for string
+full_name = str_data + ' '+ "kumari"
+print("full name  is  = ",full_name)
+
+# we can not use (-),(/),(//),(**) operator  {unsupported operator}
+# can't multiply sequence by non-int of type 'str'
+# --> multiply_str = 'neha'*'op'
+#power_str = str_data ** str_data
+
+multiply_num_str = "vaisu"*5 # this works & gives vaisu 5 times
+print(multiply_num_str)
