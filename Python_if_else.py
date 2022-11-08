c1fa374c37e93ca12d215ef316a18944daaa969e
Vaishnavi-eng137 <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-08 16:13:45 +0530

    nested if else

--- Python_if_else.py
+++ Python_if_else.py
@@ -15,3 +15,16 @@ if a == 50:
     print("Yes ,a is equals to 50 !!")
 
 print("bye !!")
+
+# else if condition 
+
+marks = 30
+
+if marks < 20 :
+    print("score more" )
+elif marks >=20 and marks<30:
+    print("good to go")    
+elif marks >=30 and marks<=40:
+    print("excellent")    
+else :
+    print("please check your marks again !!")
