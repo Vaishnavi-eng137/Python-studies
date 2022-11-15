9efe56ac8ba066996994d40870aa8d5118c3d63c
VAISHNAVI KUMARI <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-15 19:39:21 +0530

    Update python_dictionary.py

--- python_dictionary.py
+++ python_dictionary.py
@@ -28,6 +28,32 @@ print(dict2['skills'][1])
 print(dict2['other details']['nation'])
 #--> accessing from a dictionary inside dictionary
 
+#How to get the keys and typecasting into a list from a dictionary
+total_keys = list(dict2.keys())
+print('Total keys in dictionary:',total_keys)
+
+#How to get values from a dictionary
+total_values = list(dict2.values())
+print('Total values in dictionary:',total_values)
+
+#How to iterate on Dictionary
+for k,l in dict2.items():
+    print("keys is",k,'and value is',l)
+
+#Compare dictionary 
+#--> gives true bcz it compares the values with keys not by sequence
+dict3 ={'n':'polo',9:90}
+dict4 = {9:90,'n':'polo'}
+print(dict3==dict4)
+
+#--> gives false bcz value is not same
+dict3 ={'n':'polo',9:90}
+dict4 = {9:90,'n':'pl'}
+print(dict3==dict4)
+
+#How to delete specific key value pair in dictionary --> deletes the object and its references
+del(dict2['age'])
+del(dict2[56])
 
 def factorial(n):
     if n==0 or n==1:
@@ -38,3 +64,10 @@ def factorial(n):
     return result  
   
 print(factorial(3))
+
+#How to check if keys exists in dictionary
+keys_in_dict = list(dict2.keys())
+if 'skills' in keys_in_dict:
+    print(True)
+else:
+    print(False)    
