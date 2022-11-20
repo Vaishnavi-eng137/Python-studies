1530b16ae19fbdee8eaaebf24cb1a5c2819aee76
VAISHNAVI KUMARI <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-20 22:14:16 +0530

    Update python_lambda_function.py
    map method

--- python_lambda_function.py
+++ python_lambda_function.py
@@ -32,3 +32,23 @@ lambda_calc_max_two = lambda x,y :x if x>y else y
 a = 6
 b = 8
 print(lambda_calc_max_two(a,b))
+
+#How to work with map(),filter(),reduce()
+
+#implement map function
+list1 = [1,2,3,4,5,6,7,8]
+
+#Result
+#expected result = [1,4,9,16,25,36,49,64]
+
+square_num = lambda x:x*x
+square_list = list(map(square_num,list1))
+print(square_list)
+
+#Add sequential respective elements in two given lists
+list_a = [9,7,6]
+list_b = [6,4,3]
+
+sum_two_list = lambda x,y: x+y
+sequence_list = list(map(sum_two_list,list_a,list_b))
+print(sequence_list)
