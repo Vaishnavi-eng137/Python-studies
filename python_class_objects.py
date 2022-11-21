d67eb1b35b15cfca87f18cff7e3f86c776c1f538
VAISHNAVI KUMARI <72334116+Vaishnavi-eng137@users.noreply.github.com>
2022-11-21 22:01:30 +0530

    Update python_class_objects.py

--- python_class_objects.py
+++ python_class_objects.py
@@ -1,1 +1,25 @@
+class Employee:
+
+    # Constructor of class
+    # it is mainly used for assignment of instance variables
+    def __init__(self,name,salary):
+    #instance variables or instance attributes
+       self.emp_name = name           #self (means current object) pointing to each object
+       self.emp_salary = salary
+
+    #method of a class
+    def displayEmployeeInfo(self):
+        print("Employee name: ",self.emp_name,",Employee Salary: ",self.emp_salary)
+
+#How we create a object
+emp1 = Employee("Shalu",1000)
+emp2 = Employee("Neha",3000)
+
+emp1.displayEmployeeInfo()
+emp2.displayEmployeeInfo()
+
+print(emp1.emp_name)
+print(emp2.emp_name)
+
+#Difference between class variable and instance variable
 
